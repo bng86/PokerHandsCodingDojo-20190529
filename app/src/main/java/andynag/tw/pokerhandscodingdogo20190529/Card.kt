514cba1ef8276973bf21fdb318ce9bccb1c59149
andyang <yang.bng86@gmail.com>
2019-05-29 20:34:54 +0800

    teng

--- app/src/main/java/andynag/tw/pokerhandscodingdogo20190529/Card.kt
+++ app/src/main/java/andynag/tw/pokerhandscodingdogo20190529/Card.kt
@@ -1,11 +1,15 @@
 package andynag.tw.pokerhandscodingdogo20190529
 
-class Card(val suit: String = "Heart",
+class Card(val suit: String = "Hearts",
            val number: Int = 2) {
 
     companion object{
         fun create(input: String) : Card {
-            return Card()
+            if(input.substring(0) == "H") {
+                return Card("Hearts",2)
+            }else{
+                return Card("Clubs", 10)
+            }
         }
     }
 
