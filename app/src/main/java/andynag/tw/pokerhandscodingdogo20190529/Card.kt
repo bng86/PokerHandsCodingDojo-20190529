1165c9ee76d57134af7d15fd6e4713014c4c1295
andyang <yang.bng86@gmail.com>
2019-05-29 20:50:26 +0800

    bruce

--- app/src/main/java/andynag/tw/pokerhandscodingdogo20190529/Card.kt
+++ app/src/main/java/andynag/tw/pokerhandscodingdogo20190529/Card.kt
@@ -1,15 +1,17 @@
 package andynag.tw.pokerhandscodingdogo20190529
 
-class Card(val suit: String = "Hearts",
+class Card(val suit: String= "Hearts",
            val number: Int = 2) {
 
     companion object{
         fun create(input: String) : Card {
             val suit = input[0]
-            val number =input.substring(1, input.length)
+            val number =input.substring(1, input.length).toInt()
 
-            when(suit) {
-
+            return when(suit) {
+                'H'-> Card("Hearts",number )
+                'C'->Card("Clubs",number )
+                else->Card("error",0)
             }
             if(input.substring(0) == "H") {
                 return Card("Hearts",2)
@@ -21,13 +23,12 @@ class Card(val suit: String = "Hearts",
 
 }
 
-sealed class Suit() {
+enum class Suit {
     /*
      * Spade = 3
      * Heart = 2
      * Diamond = 1
      * Club = 0
      */
-    object Hearts
-    object Clubs
+    Hearts, Clubs
 }
